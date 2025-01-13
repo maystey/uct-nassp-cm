5d22e45b82fcdb384b0d215ec13245a4c77e4591
Mayhew Steyn <mayhrunes@gmail.com>
2025-01-13 16:43:54 +0200

    Add exercise and page numbering.

--- book/_ext/docnum.py
+++ book/_ext/docnum.py
@@ -4,25 +4,14 @@ from docutils import nodes
 
 from sphinx.application import Sphinx
 from sphinx.util.docutils import SphinxDirective, SphinxRole
-# from sphinx.util.typing import ExtensionMetadata
+from sphinx.util.typing import ExtensionMetadata
 
 class DocNumRole(SphinxRole):
-    # doc_count = 0
     doc_num = [0]
-
-    # def _find_level(self, tree, target, level=0) -> int:
-    #     for node in tree.children:
-    #         if isinstance(node, nodes.reference) and node['refuri'] == f'{target}.html':
-    #             return level
-    #     return -1
 
     def run(self) -> tuple[list[nodes.Node], list[nodes.system_message]]:
 
         text = ''
-
-        # if self.doc_num == 0:
-        #     print('DOC TOCS:')
-        #     print(self.env.tocs.get(self.env.docname))
 
         for cmd in self.text.split(' '):
             if cmd == 'step-super':
@@ -34,20 +23,17 @@ class DocNumRole(SphinxRole):
             elif cmd == 'step':
                 self.doc_num[-1] += 1
             elif cmd == 'step-sub':
-                self.doc_num.append(0) #Should I set this to 1?
+                self.doc_num.append(0)
             elif cmd == 'ref':
                 text += ('{}.' * len(self.doc_num))[:-1].format(*self.doc_num)
             elif cmd == 'ref-super':
                 text += ('{}.' * (len(self.doc_num) - 1))[:-1].format(*self.doc_num[:-1])
 
         node = nodes.inline(text=text)
-        # self.doc_count += 1
-        # node = nodes.inline(text=f'{self.doc_count}')
 
         return [node], []
     
-def setup(app : Sphinx): #-> ExtensionMetadata:
-    print('DOCNUM SETUP #######################################################################################')
+def setup(app : Sphinx) -> ExtensionMetadata:
     app.add_role('docnum', DocNumRole())
 
     return {
